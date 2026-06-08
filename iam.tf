8562f171105631b26399bf28aee82055c810c70a
Mohammed Abbas <abbas-97@outlook.com>
2026-06-08 15:06:32 +0100

    fix

--- iam.tf
+++ iam.tf
@@ -1,8 +1,3 @@
-resource "aws_iam_instance_profile" "nat_instance_profile" {
-  name = "mat_instance_profile"
-  role = aws_iam_role.nat_role.name
-}
-
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
@@ -14,14 +9,36 @@ data "aws_iam_policy_document" "assume_role" {
 
     actions = ["sts:AssumeRole"]
   }
-  statement {
-    effect  = "Allow"
-    actions = ["s3:*"]
-  }
 }
 
 resource "aws_iam_role" "nat_role" {
-  name               = "mat_role"
+  name               = "nat_role"
   path               = "/"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_instance_profile" "nat_instance_profile" {
+  name = "nat_instance_profile"
+  role = aws_iam_role.nat_role.name
+}
+
+resource "aws_iam_policy" "nat_s3_policy" {
+  name        = "nat_s3_policy"
+  description = "Allow S3 access from NAT instance"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:*"]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "nat_s3_attach" {
+  role       = aws_iam_role.nat_role.name
+  policy_arn = aws_iam_policy.nat_s3_policy.arn
 }
