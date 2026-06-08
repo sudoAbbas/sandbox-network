dfa3b755675512ed9a6ed7618bfc5526ee4b94ca
Mohammed Abbas <abbas-97@outlook.com>
2026-06-08 07:53:35 +0100

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.sandbox.id
+}
 
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "availability_zones" {
+  value = data.aws_availability_zones.available.names
+}
