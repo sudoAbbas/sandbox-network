4282b7ba6c1ecd1ab0f90b155caf3ab1ba268469
Mohammed Abbas <abbas-97@outlook.com>
2026-06-09 20:29:19 +0100

    found fix using command ip addr and ip route

--- nat.tf
+++ nat.tf
@@ -23,7 +23,8 @@ echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 
 dnf install -y iptables-services
 
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PRIMARY_IF=$(ip route | grep default | awk '{print $5}')
+iptables -t nat -A POSTROUTING -o $PRIMARY_IF -j MASQUERADE
 
 service iptables save
 systemctl enable iptables
